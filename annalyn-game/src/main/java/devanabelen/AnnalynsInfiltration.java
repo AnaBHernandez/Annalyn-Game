b764e0b23806e925a1741372e3e412fa16c6f0f8
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-02-17 15:58:30 +0100

    Agregados tests para la infiltración de Annalyn y modificada la lógica del juego.

--- annalyn-game/src/main/java/devanabelen/AnnalynsInfiltration.java
+++ annalyn-game/src/main/java/devanabelen/AnnalynsInfiltration.java
@@ -4,4 +4,8 @@ public class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
         return !knightIsAwake;
     }
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
 }
--- annalyn-game/src/test/java/devanabelen/AnnalynsInfiltrationTest.java
+++ annalyn-game/src/test/java/devanabelen/AnnalynsInfiltrationTest.java
@@ -1,11 +1,19 @@
 package devanabelen;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class AnnalynsInfiltrationTest {
+
     @Test
     void testCanFastAttack() {
         assertFalse(AnnalynsInfiltration.canFastAttack(true));
         assertTrue(AnnalynsInfiltration.canFastAttack(false));
+    }
+
+    @Test
+    void testCanSpy() {
+        assertTrue(AnnalynsInfiltration.canSpy(true, false, false));
+        assertFalse(AnnalynsInfiltration.canSpy(false, false, false));
     }
 }
